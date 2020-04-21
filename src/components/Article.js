50bf534d8bb173874c7ef225eada08c0210945a7
wevak <6236vic@gmail.com>
2020-04-21 18:14:42 +0530

    Implemented Markdown in the output

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,11 @@
 import React from 'react'
+const ReactMarkdown = require('react-markdown')
 
 const Article = props => {
   const showActiveOnLoad = props.indx !== 0 ? '' : 'show active'  //First article description is shown as active by default
   return (
-    <div className={`tab-pane fade text-justify ${showActiveOnLoad}`} id={`list-${props.article.id}`} role="tabpanel" aria-labelledby="list-settings-list">
-      {props.article.description}
+    <div key={props.article.id} className={`tab-pane fade text-justify ${showActiveOnLoad}`} id={`list-${props.article.id}`} role="tabpanel" aria-labelledby="list-settings-list">
+      <ReactMarkdown source={props.article.description} />
     </div>
   )
 }
